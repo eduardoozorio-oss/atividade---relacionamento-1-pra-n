3ecb93104e5db7b3d2d1d499a42d76d7c189bb39
eduardo furtado <eduardo.ozorio@aluo.senai.br>
2026-04-06 16:21:33 -0300

    add inscrição e melhorando class motorista

--- exercicio.py
+++ exercicio.py
@@ -3,8 +3,26 @@ from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 
 Base = declarative_base()
 
+
+
+inscricao = Table(
+    "inscricoes", # nome da tabela
+    Base.metadata, #Registro da tabela para ser criado com create_all
+    Column("motorista_id", Integer, ForeignKey("motorista.id"), primary_key=True),
+    Column("viagens_id", Integer, ForeignKey("viagens.id"), primary_key=True)
+)
+
 class Motorista(Base):
     __tablename__ = "motorista"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     nome = Column(String(100), nullable=False)
+
+    viagens = relationship("Viagens", secondary=inscricao, back_populates="estudantes")
+
+
+    def __repr__(self):
+        return f"- Motorista: id= {self.id} - nome: {self.nome}"
+    
+
+
